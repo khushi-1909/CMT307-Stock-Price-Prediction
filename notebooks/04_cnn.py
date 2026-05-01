364e82004cf8104b2c2ebf10ee432e83a25fd435
Tom <tr664@bath.ac.uk>
2026-05-01 09:30:17 +0100

    #3: remove reduntant backtest functions

--- notebooks/04_cnn.py
+++ notebooks/04_cnn.py
@@ -62,37 +62,6 @@ from trading import simulate_trading, baseline, plot_trading_results
 
 ### FUNCTIONS ####
 
-def predict_90(train, test, predictors, model, engine = 'sklearn'):
-
-
-    model.fit(train[predictors], train["Target"],  shuffle=True, batch_size=30)
-    if engine == 'keras':
-        probs = model.predict(test[predictors])[:,0]
-    else: 
-        probs = model.predict_proba(test[predictors])[:,1]
-    probs = pd.Series(probs, index=test.index) # Convert numpy array to pandas Series
-    preds = (probs > 0.51).astype(int)
-    preds = pd.Series(preds, index=test.index)
-    combined = pd.concat({"Target": test["Target"],"Predictions": preds, 'Probabilities': probs}, axis=1)
-    return combined
-
-def backtest_90(data, model, predictors, start=2500, step=360, engine = 'sklearn'):
-    data = data.copy()
-    all_predictions = []
-    oob_scores = []
-
-    for i in range(start, data.shape[0], step):
-        train = data.iloc[0:i].copy()
-        test = data.iloc[i:(i+step)].copy()
-        predictions = predict_90(train, test, predictors, model, engine = engine)
-
-        all_predictions.append(predictions)
-
-        if hasattr(model, "oob_score_"):
-            oob_scores.append(model.oob_score_)
-
-    return pd.concat(all_predictions), oob_scores
-
 
 def make_target(ticker_data):
   ticker_data = ticker_data[['High', 'Low', 'Open', 'Close', 'Volume']]
